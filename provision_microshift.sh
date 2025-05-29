cf8316052f37f67f7e6a419ecd84cc573d0c7728
Evgeny Slutsky <eslutsky@redhat.com>
2025-05-29 14:21:24 +0200

    first commit
    Signed-off-by: Evgeny Slutsky <eslutsky@redhat.com>

---
+++ provision_microshift.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+# Function to run the playbook with specified tags
+
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+# Source the venv using the script's directory
+source "${SCRIPT_DIR}/venv/bin/activate"
+
+run_playbook() {
+  local mode="$1"
+  local playbook="${SCRIPT_DIR}/plays/main.yaml"
+  local tags=""
+  local instance_ids=""
+
+  case "$mode" in
+    "provision")
+      playbook="${SCRIPT_DIR}/plays/provision-vm.yaml"
+      ;;
+    "deprovision")
+      playbook="${SCRIPT_DIR}/plays/deprovision-vm.yaml"
+      ;;
+    "cleanup")
+      playbook="${SCRIPT_DIR}/plays/cleanup-old.yaml"
+      echo "cleanup mode"
+      ;;
+    *)
+      echo "Invalid mode: $mode.  Defaulting to configure."
+      ;;
+  esac
+
+  # Get instance information from AWS (only for configure mode)
+  if [ "$mode" == "configure" ]; then
+    instances=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].[PublicDnsName,KeyName]" --output json)
+
+    # Create a temporary inventory file
+    inventory_file=$(mktemp)
+
+    # Write the inventory to the file
+    echo "[aws_hosts]" > "$inventory_file"
+    jq -r '.[] | .[] | @tsv' <<< "$instances" | while IFS=$'\t' read -r public_dns_name key_name; do
+      echo "$public_dns_name ansible_ssh_user=ec2-user ansible_ssh_private_key_file=/path/to/your/keys/$key_name.pem" >> "$inventory_file"
+    done
+    ansible-playbook -i "$inventory_file" "$playbook"
+    rm "$inventory_file"
+  elif [ "$mode" == "cleanup" ]; then
+    DEPROVISION_INSTANCE_IDS=$instance_ids ansible-playbook "$playbook"
+  else
+
+
+    ansible-playbook "$playbook"
+  fi
+}
+
+# Get the mode from the first argument, default to "configure"
+mode="${1:-configure}"
+run_playbook "$mode"
---
+++ scripts/install-elk.sh
@@ -0,0 +1,317 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+export KUBECONFIG=${SCRIPT_DIR}/../upstream/fetched_kubeconfigs/kubeconfig
+
+cat <<EOF | oc apply -f -
+kind: PersistentVolumeClaim
+apiVersion: v1
+metadata:
+  name: elasticsearch-data-quickstart-es-default-0
+spec:
+  accessModes:
+  - ReadWriteOnce
+  resources:
+    requests:
+      storage: 1Gi
+  storageClassName: topolvm-provisioner
+EOF
+
+
+
+# source https://surajsoni3332.medium.com/setting-up-elk-stack-on-kubernetes-a-step-by-step-guide-227690eb57f4
+
+oc create -f https://download.elastic.co/downloads/eck/2.2.0/crds.yaml
+oc apply -f https://download.elastic.co/downloads/eck/2.2.0/operator.yaml
+
+cat <<EOF | oc apply -f -
+apiVersion: elasticsearch.k8s.elastic.co/v1
+kind: Elasticsearch
+metadata:
+  name: quickstart
+spec:
+  version: 8.13.4
+  nodeSets:
+  - name: default
+    count: 1
+    config:
+      node.store.allow_mmap: false
+EOF
+
+
+# oc port-forward service/quickstart-es-http 9200
+# oc port-forward service/quickstart-kb-http 5601
+
+
+cat <<EOF | oc apply -f -
+apiVersion: kibana.k8s.elastic.co/v1
+kind: Kibana
+metadata:
+  name: quickstart
+spec:
+  version: 8.13.4
+  count: 1
+  elasticsearchRef:
+    name: quickstart
+EOF
+
+cat <<EOF | oc apply -f -
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: logstash
+  labels:
+    app.kubernetes.io/name: elasticsearch-logstash
+    app.kubernetes.io/component: logstash
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app.kubernetes.io/name: elasticsearch-logstash
+      app.kubernetes.io/component: logstash
+  template:
+    metadata:
+      labels:
+        app.kubernetes.io/name: elasticsearch-logstash
+        app.kubernetes.io/component: logstash
+    spec:
+      containers:
+        - name: logstash
+          image: docker.elastic.co/logstash/logstash:8.13.4
+          ports:
+            - name: "tcp-beats"
+              containerPort: 5044
+          env:
+            - name: ES_HOSTS
+              value: "https://quickstart-es-http.default.svc:9200"
+            - name: ES_USER
+              value: "elastic"
+            - name: ES_PASSWORD
+              valueFrom:
+                secretKeyRef:
+                  name: quickstart-es-elastic-user
+                  key: elastic
+          volumeMounts:
+            - name: config-volume
+              mountPath: /usr/share/logstash/config
+            - name: pipeline-volume
+              mountPath: /usr/share/logstash/pipeline
+            # - name: ca-certs
+            #   mountPath: /etc/logstash/certificates
+            #   readOnly: true
+      volumes:
+        - name: config-volume
+          configMap:
+            name: logstash-config
+        - name: pipeline-volume
+          configMap:
+            name: logstash-pipeline
+        # - name: ca-certs
+        #   secret:
+        #     secretName: elasticsearch-es-http-certs-public
+ 
+---
+ 
+apiVersion: v1
+kind: Service
+metadata:
+  name: logstash
+  labels:
+    app.kubernetes.io/name: elasticsearch-logstash
+    app.kubernetes.io/component: logstash
+spec:
+  ports:
+    - name: "tcp-beats"
+      port: 5044
+      targetPort: 5044
+  selector:
+    app.kubernetes.io/name: elasticsearch-logstash
+    app.kubernetes.io/component: logstash
+EOF
+
+
+cat <<EOF | oc apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: logstash-config
+  labels:
+    app.kubernetes.io/name: elasticsearch-logstash
+    app.kubernetes.io/component: logstash
+data:
+  logstash.yml: |
+    http.host: 0.0.0.0
+    pipeline.ecs_compatibility: disabled
+  pipelines.yml: |
+    - pipeline.id: logstash
+      path.config: "/usr/share/logstash/pipeline/logstash.conf"
+ 
+  log4j2.properties: |
+    logger.logstashpipeline.name = logstash.inputs.beats
+    logger.logstashpipeline.level = error
+ 
+---
+ 
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: logstash-pipeline
+  labels:
+    app.kubernetes.io/name: elasticsearch-logstash
+    app.kubernetes.io/component: logstash
+data:
+  logstash.conf: |
+    input {
+      beats {
+        port => 5044
+      }
+    }
+    filter {
+      json {
+        source => "message"
+      }
+      prune {
+        whitelist_names => [ "msg" ]
+      }
+      mutate {
+        rename => { "msg" => "message" }
+      }
+    }
+    output {
+      if [message]  =~ "admission" {
+        elasticsearch {
+          index => "logstashadmission-%{+YYYY.MM.dd}"
+          hosts => [ "\${ES_HOSTS}" ]
+          user => "\${ES_USER}"
+          password => "\${ES_PASSWORD}"
+        }
+      }
+    }
+EOF
+
+
+cat <<EOF | oc apply -f -
+apiVersion: beat.k8s.elastic.co/v1beta1
+kind: Beat
+metadata:
+  name: quickstart
+spec:
+  type: filebeat
+  version: 8.13.4
+  # elasticsearchRef:
+  #   name: quickstart
+  # kibanaRef:
+  #   name: kibana
+  config:
+    filebeat.inputs:
+    - type: container
+      paths:
+      - /var/log/containers/*.log
+      processors:
+        - add_kubernetes_metadata:
+            host: \${NODE_NAME}
+            matchers:
+            - logs_path:
+                logs_path: "/var/log/containers/"
+        - drop_event.when:
+            or:
+            - equals:
+                kubernetes.namespace: "kube-system"
+            - equals:
+                kubernetes.namespace: "kube-public"  
+            - equals:
+                kubernetes.namespace: "quickstart"
+            - equals:
+                kubernetes.namespace: "kube-node-lease"
+            - equals:
+                kubernetes.namespace: "elastic-system"
+    output.logstash:
+      hosts: ["logstash.default.svc:5044"]
+  daemonSet:
+    podTemplate:
+      spec:
+        serviceAccountName: filebeat
+        automountServiceAccountToken: true
+        terminationGracePeriodSeconds: 30
+        tolerations:
+        - key: dedicated
+          operator: Exists
+          effect: NoSchedule       
+        dnsPolicy: ClusterFirstWithHostNet
+        hostNetwork: true # Allows to provide richer host metadata
+        containers:
+        - name: filebeat
+          securityContext:
+            runAsUser: 0
+            # If using Red Hat OpenShift uncomment this:
+            #privileged: true
+          volumeMounts:
+          - name: varlogcontainers
+            mountPath: /var/log/containers
+          - name: varlogpods
+            mountPath: /var/log/pods
+          - name: varlibdockercontainers
+            mountPath: /var/lib/docker/containers
+          env:
+            - name: NODE_NAME
+              valueFrom:
+                fieldRef:
+                  fieldPath: spec.nodeName
+          resources:
+            limits:
+              cpu: 500m
+              memory: 2000Mi
+            requests:
+              cpu: 100m
+              memory: 200Mi
+        volumes:
+        - name: varlogcontainers
+          hostPath:
+            path: /var/log/containers
+        - name: varlogpods
+          hostPath:
+            path: /var/log/pods
+        - name: varlibdockercontainers
+          hostPath:
+            path: /var/lib/docker/containers
+ 
+---
+ 
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: filebeat
+rules:
+- apiGroups: [""] # "" indicates the core API group
+  resources:
+  - namespaces
+  - pods
+  - nodes
+  verbs:
+  - get
+  - watch
+  - list
+ 
+---
+ 
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: filebeat
+subjects:
+- kind: ServiceAccount
+  name: filebeat
+  namespace: elk-test
+roleRef:
+  kind: ClusterRole
+  name: filebeat
+  apiGroup: rbac.authorization.k8s.io
+ 
+---
+ 
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: filebeat
+EOF
